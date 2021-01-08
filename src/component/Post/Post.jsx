559470c26de8814954daf81ba235f1e3172efa0a
re1fly <reflyfa@gmail.com>
2021-01-08 14:59:09 +0800

    CRUD data json with axios

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Card, CardColumns } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, CardColumns, Button } from 'react-bootstrap';
 
 const Post = (props) => {
     return (
@@ -11,12 +10,14 @@ const Post = (props) => {
                         Data Dummy
                     </Card.Title>
                 </Card.Header>
-                <Card.Body>
-                    <div className="img-thumb">
+                <Card.Body className="text-center">
+                    <div className="mb-3">
                         <img src="http://placeimg.com/200/150/tech" alt="dummy" />
                     </div>
-                        <p className="font-weight-bold">{props.title}</p>
-                        <p className="desc">{props.desc}</p>
+                        <p className="font-weight-bold">{props.data.title}</p>
+                        <p className="desc">{props.data.body}</p>
+                        <Button className='mr-4' variant="success" size="sm" onClick={() => props.update(props.data)} block>Update</Button>
+                        <Button variant="danger" size="sm" onClick={() => props.delete(props.data.id)} block>Delete</Button>
                 </Card.Body>
             </Card>
         </CardColumns>
--- src/container/BlogPostApi/BlogPostApi.jsx
+++ src/container/BlogPostApi/BlogPostApi.jsx
@@ -1,20 +1,25 @@
 import React, {Component, Fragment} from 'react';
 import Post from '../../component/Post/Post';
 import axios from 'axios';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class BlogPostApi extends Component {
     state = {
-        post: []
+        post: [],
+        formBlogPost: {
+            userId: 1,
+            id: 1,
+            title: '',
+            body: ''
+        },
+        isUpdate: false,
     }
-    componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        //     .then(response => response.json())
-        //     .then(json => {
-        //         this.setState({
-        //             post: json
-        //         })
-        //     })]
-        axios.get('http://localhost:3004/posts')
+
+
+    getPostAPI = () => {
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
             .then((response)=> {
                 console.log(response.data);
                 this.setState({
@@ -22,12 +27,99 @@ class BlogPostApi extends Component {
                 })
             })
     }
+
+    postDataToApi = () => {
+        axios.post('http://localhost:3004/posts', this.state.formBlogPost).then((response) => {
+            this.getPostAPI();
+            this.setState({
+                formBlogPost: {
+                    userId: 1,
+                    id: 1,
+                    title: '',
+                    body: ''
+        },
+            })
+        }, (error) => {
+            console.log('data error', error);
+        })
+    }
+
+    putDatatoApi = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost).then((response) => {
+            this.getPostAPI();
+            this.setState({
+                isUpdate: false,
+                formBlogPost: {
+                    userId: 1,
+                    id: 1,
+                    title: '',
+                    body: ''
+        },
+            })
+        })
+    }
+
+    handleDelete = (data) =>{
+        axios.delete(`http://localhost:3004/posts/${data}`).then((response)=>{
+            this.getPostAPI()
+        })
+    }
+
+    handleUpdate = (data) =>{
+        console.log(data);
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
+    }
+
+    handleFormChange = (event) => {
+        // console.log('form was change', event.target)
+        let newFormBlogPost = {...this.state.formBlogPost};
+        let timestamp = new Date().getTime();
+        if(!this.state.isUpdate){
+            newFormBlogPost['id'] = timestamp;
+        }
+        newFormBlogPost[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: newFormBlogPost
+        })
+
+    }
+  
+    handleSubmit = () => {
+        if(this.state.isUpdate){
+            this.putDatatoApi();
+        }else{
+            this.postDataToApi();
+        }
+    }
+
+    componentDidMount() {
+        this.getPostAPI()
+    }
     render() {
         return (
             <Fragment>
+                <Card style={{ width: '40%' }}>
+                    <Card.Header className="text-center font-weight-bold" >Insert Data</Card.Header>
+                    <Card.Body>
+                <Form>
+                <Form.Group controlId="formGroupTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control  type="text" value={this.state.formBlogPost.title} name="title" placeholder="Enter Title" onChange={this.handleFormChange} />
+                </Form.Group>
+                <Form.Group controlId="formGroupDescription">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" value={this.state.formBlogPost.body} name="body" placeholder="Add Description" onChange={this.handleFormChange} />
+                </Form.Group>
+                <Button variant="primary" onClick={this.handleSubmit}>Submit</Button>
+                </Form>
+                </Card.Body>
+                </Card><br />
                 {
                     this.state.post.map( post => {
-                       return <Post key={post.id} title={post.title} desc={post.body} />
+                       return <Post key={post.id} data={post} delete={this.handleDelete} update={this.handleUpdate}  />
                     })
                 }
             </Fragment>
